19fd7e3e839c6ad35990fdba999817bc7631ceef
brucemack <bruce@mackinnon.com>
2023-12-28 21:38:54 +0000

    Working on RTTY

--- rtty/BaudotEncoder.cpp
+++ rtty/BaudotEncoder.cpp
@@ -41,6 +41,9 @@ void transmitBaudot(const char* msg, FSKModulator& mod, uint32_t symbolLengthUs)
     // Send a short mark at the beginning so that we can see the 
     // transition of the initial start bit.    
     mod.sendMark(symbolLengthUs);
+    mod.sendMark(symbolLengthUs);
+    mod.sendMark(symbolLengthUs);
+    mod.sendMark(symbolLengthUs);
 
     // We are assuming that the receiver always starts in LTRS mode
     BaudotMode mode = BaudotMode::LTRS;
