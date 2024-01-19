347134be7b70ff91597f0e6bf55b3b52243ceca3
Bruce MacKinnon <bruce@mackinnon.com>
2024-01-19 06:46:17 -0500

    GSM encoder - WIP

--- gsm-06.10/Encoder.cpp
+++ gsm-06.10/Encoder.cpp
@@ -19,6 +19,37 @@ static uint16_t k2zone(uint16_t k) {
     }
 }
 
+// See table 5.1 on page 43
+// NOTE: The 0th entry is not used.  The draft uses index [1..8]
+static const int16_t A[9] = { 0, 20480, 20480, 20480, 20480, 13964, 15360, 8534, 9036 };
+static const int16_t B[9] = { 0, 0, 0, 2048, -2560, 94, -1792, -341, -1144 };
+static const int16_t MIC[9] = { 0, -32, -32, -16, -16, -8, -8, -4, -4 };
+static const int16_t MAC[9] = { 0, 31, 31, 15, 15, 7, 7, 3, 3 };
+
+// See table 5.2 on page 43
+// NOTE: The 0th entry is not used.  The draft uses index [1..8]
+static const int16_t INVA[9] = { 0, 13107, 13107, 13107, 13107, 19223, 17476, 31454, 29708 };
+
+// Table 5.3a: Decision level of the LTP gain quantizer
+// See page 43
+static const int16_t DLB[4] = { 6554, 16384, 26214, 32767 };
+
+// Table 5.3b: Quantization levels of the LTP gain quantizer
+// See page 43
+static const int16_t QLB[4] = { 3277, 11469, 21299, 32767 };
+
+// Table 5.4: Coefficients of the weighting filter
+// See page 43
+static const int16_t H[11] = { -134, -374, 0, 2054, 5741, 8192, 5741, 2054, 0, -374, -134 };
+
+// Table 5.5: Normalized inverse mantissa used to compute xM/xmax
+// See page 44
+static const int16_t NRFAC[8] = { 29128, 26215, 23832, 21846, 20165, 18725, 17476, 16384 };
+
+// Table 5.6: Normalized direct mantissa used to compute xM/xmax
+// See page 44
+static const int16_t FAC[8] = { 18431, 20479, 22527, 24575, 26623, 28671, 30719, 32767 };
+
 Encoder::Encoder() {
     reset();
 }
@@ -231,6 +262,7 @@ void Encoder::encode(const int16_t sop[], uint8_t out[]) {
     // Section 5.2.7. - Quantization and coding of the Log-Area Ratios
 
     // Computation for quantizing and coding the LAR[1..8]
+    // A[], B[] are lookup tables 5.1
     for (uint16_t i = 1; i <= 8; i++) {
         temp = mult(A[i], LAR[i]);
         temp = add(temp, B[i]);
@@ -241,10 +273,10 @@ void Encoder::encode(const int16_t sop[], uint8_t out[]) {
         if (LARc[i] > MAC[i]) {
             LARc[i] = MAC[i];
         } else if (LARc[i] < MIC[i]) {
-            LARc[i] = MIN[i];
+            LARc[i] = MIC[i];
         }
         // NOTE: The equation is used to make all of the LARc[i] positive
-        LARc[i] = sub(LARc[i], MIN[i]);
+        LARc[i] = sub(LARc[i], MIC[i]);
     }
 
     // ===== SHORT TERM ANALYSIS FILTERING SECTION ===========================
